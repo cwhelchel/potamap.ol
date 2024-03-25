b49b14143a94336a762cb3455b747dce30211ec3
Cainan Whelchel <c.whelchel@gmail.com>
2024-03-25 10:10:33 -0400

    remove vite-plugin-mkcert to fix deploy. update package-lock

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   build: {
     sourcemap: true,
   },
   base: '/potamap.ol/',
-  server: { https: true },
-  plugins: [ mkcert() ]
 })
